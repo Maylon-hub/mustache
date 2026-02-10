2266bca76d5bd5a726d93722c1e92ec9ddcf103b
Maylon <maylonmartins@estudante.ufscar.br>
2026-02-10 16:43:20 -0300

    feat: add PowerShell and Bash scripts to start MustaCHE v2 using Docker Compose

---
+++ run.ps1
@@ -0,0 +1,3 @@
+docker compose up --build -d
+echo "MustaCHE v2 is starting..."
+echo "Access the application at: http://localhost:5001"
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker compose up --build -d
+echo "MustaCHE v2 is starting..."
+echo "Access the application at: http://localhost:5001"
